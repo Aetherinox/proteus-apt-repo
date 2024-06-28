f4dfa6cba9a43560a14fc9c314e2118c069b83ce
Aetherinox <aetherinox@proton.me>
2024-06-28 00:00:18 -0700

    [S] auto-update [ jammy ] @ 06.28.2024 00:00:07

--- proteus-git.sh
+++ proteus-git.sh
@@ -945,15 +945,6 @@ spin()
 {
     spinner="-\\|/-\\|/"
 
-    while :
-    do
-        for i in `seq 0 7`
-        do
-            echo -n "${spinner:$i:1}"
-            echo -en "\010"
-            sleep 0.4
-        done
-    done
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
